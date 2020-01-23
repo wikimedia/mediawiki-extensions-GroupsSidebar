a5cbaca41ca08cd83d11c43edebb4cd2dce7beac
DannyS712 <DannyS712.enwiki@gmail.com>
2020-01-23 17:17:58 +0000

    Remove use of global $wgUser
    Bug: T243531
    Change-Id: I33209d9da8a1e23a6347498a0dd5cc33d8db8ffa

--- GroupsSidebar.body.php
+++ GroupsSidebar.body.php
@@ -5,8 +5,7 @@ class GroupsSidebar {
 	 * Gets called by Hook SkinBuildSidebar
 	 */
 	public static function efHideSidebar( $skin, &$bar ) {
-		global $wgUser;
-		foreach ( $wgUser->getEffectiveGroups() as $group ) {
+		foreach ( $skin->getUser()->getEffectiveGroups() as $group ) {
 			$message = 'sidebar-'.$group;
 			# addToSidebar currently won't throw errors if we call it
 			# with nonexisting pages, but better check and be sure
