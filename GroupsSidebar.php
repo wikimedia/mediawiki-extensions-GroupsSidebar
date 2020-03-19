5ddb8ed3c7c315fe2a49fb0ec70515ddbe953ae2
zoranzoki21 <zorandori4444@gmail.com>
2020-03-19 15:33:13 +0100

    Add phpcs and make it pass
    Change-Id: Ic6f48b0bf242d5db9380adfb93263357cabb4c8c

--- GroupsSidebar.php
+++ GroupsSidebar.php
@@ -3,14 +3,19 @@
 class GroupsSidebar {
 	/**
 	 * Gets called by Hook SkinBuildSidebar
+	 *
+	 * @param Skin $skin
+	 * @param Sidebar &$bar
+	 * @return bool|array
 	 */
 	public static function efHideSidebar( $skin, &$bar ) {
 		foreach ( $skin->getUser()->getEffectiveGroups() as $group ) {
-			$message = 'sidebar-'.$group;
+			$message = 'sidebar-' . $group;
 			# addToSidebar currently won't throw errors if we call it
 			# with nonexisting pages, but better check and be sure
-			if ( Title::newFromText( $message, NS_MEDIAWIKI )->exists() )
+			if ( Title::newFromText( $message, NS_MEDIAWIKI )->exists() ) {
 				$skin->addToSidebar( $bar, $message );
+			}
 		}
 		return true;
 	}
